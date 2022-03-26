32e6fb790320cdc63df141922baa5b67a98bfd95
Sunny996 <satlaakash@gmail.com>
2022-03-26 23:25:46 +0530

    ADDED: A method to Binary.java file to swap nibbles and find the new number

--- src/main/Test.java
+++ src/main/Test.java
@@ -6,6 +6,10 @@ public class Test {
 //        System.out.println("The day of the week is "+JUnitTestingPrograms.dayOfWeek(10,6,1998));
 //        System.out.println(JUnitTestingPrograms.tempConversion(32));
 //        System.out.println(JUnitTestingPrograms.monthlyPayment(Double.parseDouble(args[0]),Double.parseDouble(args[1]),Double.parseDouble(args[2])));
-        System.out.println(JUnitTestingPrograms.sqrt(100));
+//        System.out.println(JUnitTestingPrograms.sqrt(100));
+//        System.out.println("The day of the week is "+JUnitTestingPrograms.dayOfWeek(10,6,1998));
+//        System.out.println(JUnitTestingPrograms.toBinary(100));
+       Binary.nibbleSwap(100);
     }
+
 }
