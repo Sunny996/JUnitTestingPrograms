9d6cef12ffaa1782a5b9a90c1d0fb0ae66bb6a74
Sunny996 <satlaakash@gmail.com>
2022-03-26 22:06:33 +0530

    ADDED: A method to find the day of week of a date

--- src/main/JUnitTestingPrograms.java
+++ src/main/JUnitTestingPrograms.java
@@ -30,4 +30,36 @@ public class JUnitTestingPrograms {
         System.out.println();
         return minNoOfNotes;
     }
+
+    public static String dayOfWeek(int m, int d, int y) {
+        String day = "";
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + 31 * m0 / 12) % 7;
+        switch (d0) {
+            case 0:
+                day = "Sunday";
+                break;
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+        }
+        return day;
+    }
 }
