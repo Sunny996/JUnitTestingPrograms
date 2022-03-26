032ec2d0e579c30a26ad4c7d458a5f74ae4bba33
Akash Satla <65150847+Sunny996@users.noreply.github.com>
2022-03-26 23:05:17 +0530

    Update Test.java

--- src/main/Test.java
+++ src/main/Test.java
@@ -7,6 +7,7 @@ public class Test {
 //        System.out.println(JUnitTestingPrograms.tempConversion(32));
 //        System.out.println(JUnitTestingPrograms.monthlyPayment(Double.parseDouble(args[0]),Double.parseDouble(args[1]),Double.parseDouble(args[2])));
 //         System.out.println(JUnitTestingPrograms.sqrt(100));
-        System.out.println("The day of the week is "+JUnitTestingPrograms.dayOfWeek(10,6,1998));
+//         System.out.println("The day of the week is "+JUnitTestingPrograms.dayOfWeek(10,6,1998));
+        System.out.println(JUnitTestingPrograms.toBinary(100));
     }
 }
