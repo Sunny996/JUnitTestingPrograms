72e82653fea843efde49d61bb045a0775bf55b6a
Sunny996 <satlaakash@gmail.com>
2022-03-26 23:33:08 +0530

    ADDED: A Binary.java class to swap the nibbles and find the numebr

---
+++ src/main/Binary.java
@@ -0,0 +1,20 @@
+package main;
+
+public class Binary {
+    public static void nibbleSwap(int n){
+        int newNum=0,count=0;
+        String binary="",newBinary="";
+        binary=JUnitTestingPrograms.toBinary(n);
+        newBinary=binary.substring(4,8)+binary.substring(0,4);
+        for(int i=0;i<newBinary.length();i++){
+            if(newBinary.charAt(i)=='1'){
+                newNum+=Math.pow(2,newBinary.length()-i-1);
+                count++;
+            }
+        }
+        if(count==1)
+            System.out.println("The new number is "+ newNum+ " and it is a power of 2");
+        else
+            System.out.println("The new number is "+ newNum+ " and it is not a power of 2");
+    }
+}
--- src/main/Test.java
+++ src/main/Test.java
@@ -7,7 +7,8 @@ public class Test {
 //        System.out.println(JUnitTestingPrograms.monthlyPayment(Double.parseDouble(args[0]),Double.parseDouble(args[1]),Double.parseDouble(args[2])));
 //         System.out.println(JUnitTestingPrograms.sqrt(100));
 //         System.out.println("The day of the week is "+JUnitTestingPrograms.dayOfWeek(10,6,1998));
-        System.out.println(JUnitTestingPrograms.toBinary(100));
+//        System.out.println(JUnitTestingPrograms.toBinary(100));
+        Binary.nibbleSwap(100);
     }
 
 }
